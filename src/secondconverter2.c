b00d2881556a262c9110ecf0dccf8d8c96eb520e
Henrik Christensen <sensen1695@hotmail.com>
2018-09-21 08:07:33 +0200

    removed days and weeks + updated if-statements

--- src/secondconverter2.c
+++ src/secondconverter2.c
@@ -4,15 +4,15 @@
  *
  * Prompts the user for a number of seconds
  * Then convert the entered number of seconds to
- *   weeks, days, hours, minutes and seconds
+ *   hours, minutes and seconds
  * and output the result to the user
  *
  * examples:
  *
- * if 43 seconds:      43 seconds
- * if 650 seconds:     10 minutes and 50 seconds
- * if 4000 seconds:    1 hours, 6 minutes and 40 seconds
- * if 1000000 seconds: 1 weeks, 4 days, 13 hours, 46 minutes and 40 seconds
+ * if 75 seconds:      1 minute and 15 seconds
+ * if 3700 seconds:    1 hour, 1 minute and 40 seconds
+ * if 55 seconds:      55 seconds
+ * if 3661 seconds:    1 hour, 1 minute and 1 second
 */
 
 #include <stdio.h>
@@ -21,14 +21,12 @@
 #define SECONDS_PR_MINUTE 60
 #define SECONDS_PR_HOUR 3600
 #define HOURS_PR_DAY 24
-#define DAYS_PR_WEEK 7
-#define WEEKS_PR_YEAR 52
 #define INVALID_INPUT -1
 
 int main(int argc, char *argv[])
 {
   unsigned int input;
-  unsigned int seconds, minutes, hours, days, weeks;
+  unsigned int seconds, minutes, hours;
 
   /* checks if user enter seconds in the command line */
   if (argc == 2)
@@ -61,22 +59,18 @@ int main(int argc, char *argv[])
   seconds = input % SECONDS_PR_MINUTE;
   minutes = (input / SECONDS_PR_MINUTE) % SECONDS_PR_MINUTE;
   hours = (input / SECONDS_PR_HOUR) % HOURS_PR_DAY;
-  days = (input / (SECONDS_PR_HOUR * HOURS_PR_DAY)) % DAYS_PR_WEEK;
-  weeks = (input / (SECONDS_PR_HOUR * HOURS_PR_DAY * DAYS_PR_WEEK)) % WEEKS_PR_YEAR;
 
-  if (weeks > 0)
-    printf("\n%u weeks, %u days, %u hours, %u minutes and %u seconds\n",
-	   weeks, days, hours, minutes, seconds);
-  else if (days > 0)
-    printf("\n%u days, %u hours, %u minutes and %u seconds\n",
-	   days, hours, minutes, seconds);
-  else if (hours > 0)
-    printf("\n%u hours, %u minutes and %u seconds\n",
-	   hours, minutes, seconds);
+  if (hours > 0)
+    printf("\n%u %s, %u %s and %u %s\n",
+	   hours, hours > 1 ? "hours" : "hour",
+	   minutes, minutes > 1 ? "minutes" : "minute",
+	   seconds, seconds > 1 ? "seconds" : "second");
   else if (minutes > 0)
-    printf("\n%u minutes and %u seconds\n", minutes, seconds);
+    printf("\n%u %s and %u %s\n",
+	   minutes, minutes > 1 ? "minutes" : "minute",
+	   seconds, seconds > 1 ? "seconds" : "second");
   else
-    printf("\n%u seconds\n", seconds);
+    printf("\n%u %s\n", seconds, seconds > 1 ? "seconds" : "second");
   
   return EXIT_SUCCESS;
 }
